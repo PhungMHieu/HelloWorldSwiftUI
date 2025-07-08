462daa91f1208d7903da05d3c0470fbb19ab4d76
PhungMHieu <HieuPM.B21cn368@stu.ptit.edu.vn>
2025-07-08 18:03:16 +0700

    first commit

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,16 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//        var nameField
+        VStack (spacing: 40, content: {
+            HStack(alignment: .top) {
+                ExtractedView(title: "First name", placeholder: "Enter first name")
+                ExtractedView(title: "Last name", placeholder: "Enter last name")
+            }.padding(.horizontal, 20.0)
+            GenderSelector()
+            HStack {
+                HeightWeightView(title: "Weight", placeholder: "Enter weight")
+                HeightWeightView(title: "Height", placeholder: "Enter height")
+//                HeightWeightView()
+//                HeightWeightView()
+            }
+            .padding(.horizontal, 20)
+            
+        })
     }
 }
 
+
+
+struct GenderSelector: View {
+    @State private var selectedGender = "Nam"
+    let genders = ["Nam", "Nữ", "Không"]
+    var body: some View {
+        
+//        Picker
+        Picker("GenderSelector",selection: $selectedGender) {
+            ForEach(genders, id: \.self){ gender in
+                Text(gender).tag(gender)
+            }
+        }.pickerStyle(.segmented).padding()
+    }
+}
+
+struct HeightWeightView: View {
+    @State var weightValue: String = ""
+    var title: String
+    var placeholder: String
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(title)
+            TextField(placeholder, text: $weightValue)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+    }
+}
 #Preview {
     ContentView()
+//    ContentView().previewLayout(.fixed(width: 568, height: 320))
 }
---
+++ HelloWorld/ExtractView.swift
@@ -0,0 +1,24 @@
+//
+//  ExtractView.swift
+//  HelloWorld
+//
+//  Created by iKame Elite Fresher 2025 on 8/7/25.
+//
+import SwiftUI
+
+
+struct ExtractedView: View {
+    @State var firstName: String = ""
+    var title: String
+    var placeholder: String
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Text(title)
+            //                .font(.headline)
+            TextField(placeholder, text: $firstName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding(.trailing, 8.0)
+        }
+//        .padding(.trailing, 8.0 )
+    }
+}
